b5df562c50e1afd90c4d7d929dbd370fd27a41f9
Bogdan Gersak <bogdan@gersak.ro>
2014-12-27 10:52:36 +0100

    Formatting

--- getters.go
+++ getters.go
@@ -1,9 +1,9 @@
 /*
 	Go doesn't provide automatic support for getters and setters.
-	There's nothing wrong with providing getters and setters yourself, and it's often appropriate to do so, 
-	but it's neither idiomatic nor necessary to put Get into the getter's name. If you have a field called 
-	owner (lower case, unexported), the getter method should be called Owner (upper case, exported), 
-	not GetOwner. The use of upper-case names for export provides the hook to discriminate the field from 
+	There's nothing wrong with providing getters and setters yourself, and it's often appropriate to do so,
+	but it's neither idiomatic nor necessary to put Get into the getter's name. If you have a field called
+	owner (lower case, unexported), the getter method should be called Owner (upper case, exported),
+	not GetOwner. The use of upper-case names for export provides the hook to discriminate the field from
 	the method. A setter function, if needed, will likely be called SetOwner.
 */
 package main
