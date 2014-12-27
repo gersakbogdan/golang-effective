d18f227b23cec9aeae6959be30435327ea57d28b
Bogdan Gersak <bogdan@gersak.ro>
2014-12-27 09:32:55 +0100

    Formatting example

---
+++ formatting.go
@@ -0,0 +1,22 @@
+/*
+	This file is using the standard Go format.
+	You can format a file using fmt command:
+		go fmt filename
+*/
+package main
+
+import (
+	"fmt"
+)
+
+type Number struct {
+	X int // X is an integer
+	Y int // Y is an integer
+}
+
+func main() {
+	m := Number{3}
+
+	// Prints 3
+	fmt.Println(m.X)
+}
